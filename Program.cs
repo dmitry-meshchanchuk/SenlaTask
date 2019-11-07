ea87a06a5834727f359a3fa966ded2ae6cbfde19
Dmitry Meshchanchuk <michalytch97@gmail.com>
2019-11-07 12:51:41 +0300

    feat: add Task3

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 
 namespace SenlaTask
@@ -65,6 +59,39 @@ namespace SenlaTask
     class Task3
     {
         //task in progress
+        public static string IntersectionPointSearch(double ax1, double ay1, double ax2, double ay2,
+                                                     double bx1, double by1, double bx2, double by2)
+        {
+            double v1 = (bx2 - bx1) * (ay1 - by1) - (by2 - by1) * (ax1 - bx1);
+            double v2 = (bx2 - bx1) * (ay2 - by1) - (by2 - by1) * (ax2 - bx1);
+            double v3 = (ax2 - ax1) * (by1 - ay1) - (ay2 - ay1) * (bx1 - ax1);
+            double v4 = (ax2 - ax1) * (by2 - ay1) - (ay2 - ay1) * (bx2 - ax1);
+            if ((v1 * v2 < 0) && (v3 * v4 < 0))
+            {
+                return "Пересекаются";
+            }
+            else
+                return "Не Пересекаются";
+        }
+        public void SolutionTask3()
+        {
+            //double ax1 = double.Parse(Console.ReadLine());
+            //double ax2 = double.Parse(Console.ReadLine());
+            //double ay1 = double.Parse(Console.ReadLine()); 
+            //double ay2 = double.Parse(Console.ReadLine());
+            //double bx1 = double.Parse(Console.ReadLine());
+            //double by1 = double.Parse(Console.ReadLine());
+            //double bx2 = double.Parse(Console.ReadLine());
+            //double by2 = double.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("task in progress");
+
+            Console.WriteLine();
+        }
+
+
     }
     class Task4
     {
@@ -143,22 +170,32 @@ namespace SenlaTask
             Console.WriteLine("Task1");
             Task1 n1 = new Task1();
             n1.SolutionTask1();
+            Console.ReadKey();
 
             Console.WriteLine("Task2");
             Task2 n2 = new Task2();
             n2.SolutionTask2();
+            Console.ReadKey();
+
+            Console.WriteLine("Task3");
+            Task3 n3 = new Task3();
+            n3.SolutionTask3();
+            Console.ReadKey();
 
             Console.WriteLine("Task4");
             Task4 n4 = new Task4();
             n4.SolutionTask4();
+            Console.ReadKey();
 
             Console.WriteLine("Task5");
             Task5 n5 = new Task5();
             n5.SolutionTask5();
+            Console.ReadKey();
 
             Console.WriteLine("Task6");
             Task6 n6 = new Task6();
             n6.SolutionTask6();
+            Console.ReadKey();
         }
     }
 }
